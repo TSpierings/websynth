59edc4aa580e87848272626afb2c49449c3976b7
Tchaka Spierings <tchaka.spierings@gmail.com>
2019-07-17 23:48:17 +0200

    Support touch based interaction for knobs

--- src/modules/shared/knob/knob.component.ts
+++ src/modules/shared/knob/knob.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-knob',
   templateUrl: './knob.component.html',
   styleUrls: ['./knob.component.scss']
 })
-export class KnobComponent implements OnInit {
+export class KnobComponent {
 
   @Output() valueChanged = new EventEmitter<number>();
 
@@ -18,9 +18,6 @@ export class KnobComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
   mouseDown(event: MouseEvent) {
     this.dragCoordinates = {
       original: this.knobValue,
@@ -32,19 +29,38 @@ export class KnobComponent implements OnInit {
   mouseMove(event: MouseEvent) {
     if (this.dragCoordinates) {
       this.knobValue = this.dragCoordinates.original + (this.dragCoordinates.screenY - event.screenY);
-
-      // Knob goes from 0 to 100
-      this.knobValue = Math.min(this.knobValue, 100);
-      this.knobValue = Math.max(this.knobValue, 0);
-
-      this.valueChanged.emit(this.knobValue);
+      this.limitAndEmitKnobValue();
     }
   }
 
-  mouseUp(event: MouseEvent) {
+  touchStart(event: TouchEvent) {
+    this.dragCoordinates = {
+      original: this.knobValue,
+      screenX: event.touches[0].screenX,
+      screenY: event.touches[0].screenY
+    };
+  }
+
+  touchMove(event: TouchEvent) {
+    event.preventDefault();
+
+    if (this.dragCoordinates) {
+      this.knobValue = this.dragCoordinates.original + (this.dragCoordinates.screenY - event.touches[0].screenY);
+      this.limitAndEmitKnobValue();
+    }
+  }
+
+  interactionEnd() {
     if (this.dragCoordinates) {
       this.dragCoordinates = null;
     }
   }
 
+  private limitAndEmitKnobValue() {
+      // Knob goes from 0 to 100
+      this.knobValue = Math.min(this.knobValue, 100);
+      this.knobValue = Math.max(this.knobValue, 0);
+
+      this.valueChanged.emit(this.knobValue);
+  }
 }
